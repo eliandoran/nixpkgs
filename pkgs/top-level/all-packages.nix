8f1120c011ac309f44dd1448171c2ef6f5734015
mtetreault <mathieu.alexandre.tetreault@gmail.com>
2021-04-03 21:33:20 -0400

    jlink: init at v.694

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -31809,6 +31809,8 @@ with pkgs;
     inherit (darwin.apple_sdk.frameworks) Carbon Cocoa;
   };
 
+  jlink = callPackage ../development/tools/jlink {};
+  
   katago = callPackage ../games/katago { };
 
   katagoWithCuda = katago.override {

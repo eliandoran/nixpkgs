cea2180655e4dce850dd28c1f0738c0d7afe8b59
Elian Doran <contact@eliandoran.me>
2022-01-17 18:25:54 +0200

    jlink: Fix udev rules and use latest version

--- pkgs/development/tools/jlink/default.nix
+++ pkgs/development/tools/jlink/default.nix
@@ -20,7 +20,7 @@
 }:
 
 let
-  jlinkVersion = "694";
+  jlinkVersion = "760d";
 
   architecture = {
     x86_64-linux = "x86_64";
@@ -29,7 +29,7 @@ let
   }.${stdenv.hostPlatform.system} or (throw "unsupported system ${stdenv.hostPlatform.system}");
 
   sha256 = {
-    x86_64-linux = "1y1i30y8h9pq345r25wycnfns1zz0y3g7b66a82nx3075zx2n4lm";
+    x86_64-linux = "sha256-TndqfEcEYLMSTLVhlbKcy/r4rGYb6yaUmhFLS5dWtX8=";
     i686-linux = "1vdfxiwwxxr6vjybd0xl8iq79b5j7kd10bk9j22ghkg7b4mbsjrm";
     armv7l-linux = "0hpiirzy1921fca7b0bcrmc48r03r0lv0qph6xnqdkv66iplj1gz";
   }.${stdenv.hostPlatform.system} or (throw "unsupported system ${stdenv.hostPlatform.system}");
@@ -80,7 +80,10 @@ stdenv.mkDerivation rec {
     cp -R * $out/JLink
     ln -s $out/JLink/J* $out/bin/
     rm -r $out/bin/JLinkDevices.xml $out/JLink/libQt*
-    install -D -t $out/lib/udev/rules.d 99-jlink.rules
+
+    # Install udev rules
+    mkdir -p $out/etc/udev/rules.d
+    cp ./99-jlink.rules $out/etc/udev/rules.d
   '';
 
   preFixup = ''

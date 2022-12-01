cd54f9012729a43dbb0aae597a879d91da6a0827
Elian Doran <contact@eliandoran.me>
2022-12-01 17:26:42 +0200

    Revert "video-trimmer: 0.7.1 -> 0.8.0"
    This reverts commit a6f95362e78fa8681517aa250638e3eee5de4267.

--- pkgs/applications/video/video-trimmer/default.nix
+++ pkgs/applications/video/video-trimmer/default.nix
@@ -1,6 +1,7 @@
 { stdenv
 , lib
 , fetchFromGitLab
+, fetchpatch
 , rustPlatform
 , gnome
 , pkg-config
@@ -9,34 +10,42 @@
 , desktop-file-utils
 , blueprint-compiler
 , ninja
+, gobject-introspection
 , gtk4
 , libadwaita
 , gst_all_1
-, ffmpeg-full
 }:
 
 stdenv.mkDerivation rec {
   pname = "video-trimmer";
-  version = "0.8.0";
+  version = "0.7.1";
 
   src = fetchFromGitLab {
     domain = "gitlab.gnome.org";
     owner = "YaLTeR";
-    repo = pname;
+    repo = "video-trimmer";
     rev = "v${version}";
-    hash = "sha256-0zhQoxzU1GikYP5OwqMl34RsnefJtdZox5EuTqOFnas=";
+    sha256 = "sha256-D7wjJkdqqjjwwYEUZnNr7hFQK59wfTnaCLXCy+SK8Jo=";
   };
-
   cargoDeps = rustPlatform.fetchCargoTarball {
     inherit src;
     name = "${pname}-${version}";
-    hash = "sha256-kH9AfEskh7TTXF+PZwOZNWVJmnEeMJrSEEuDGyP5A5o=";
+    hash = "sha256-cB5dVrEbISvHrOb87uVZSkT694VKtPtyk+c1tYNCTp0=";
   };
+
+  patches = [
+    # https://gitlab.gnome.org/YaLTeR/video-trimmer/-/merge_requests/12
+    (fetchpatch {
+      url = "https://gitlab.gnome.org/YaLTeR/video-trimmer/-/commit/2faf4bb13d44463ea940c39ece9187f76627dbe9.diff";
+      sha256 = "sha256-BPjwfFCDIqnS1rAlIinQ982VKdAYLyzDAPLCmPDvdp4=";
+    })
+  ];
 
   nativeBuildInputs = [
     pkg-config
     meson
     wrapGAppsHook4
+    gobject-introspection
     desktop-file-utils
     blueprint-compiler
     ninja
@@ -59,12 +68,6 @@ stdenv.mkDerivation rec {
   ];
 
   doCheck = true;
-
-  preFixup = ''
-    gappsWrapperArgs+=(
-      --prefix PATH : "${lib.makeBinPath [ ffmpeg-full ]}"
-    )
-  '';
 
   meta = with lib; {
     homepage = "https://gitlab.gnome.org/YaLTeR/video-trimmer";

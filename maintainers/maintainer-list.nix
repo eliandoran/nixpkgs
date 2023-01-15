b59ba58022924bd4e29411fa31f3a3f6617ed01a
Elian Doran <contact@eliandoran.me>
2023-01-15 12:58:55 +0200

    maintainers: add eliandoran

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -4050,6 +4050,12 @@
     githubId = 103082;
     name = "Ed Brindley";
   };
+  eliandoran = {
+    email = "contact@eliandoran.me";
+    name = "Elian Doran";
+    github = "eliandoran";
+    githubId = 21236836;
+  };
   elizagamedev = {
     email = "eliza@eliza.sh";
     github = "elizagamedev";

b416c5af32f546c3ad321723f5bcce6612bbf674
Elian Doran <contact@eliandoran.me>
2023-03-22 00:36:14 +0200

    Revert "microej-sdk: init at 22.06"
    This reverts commit 21d7355684955e0523859ac4b546ad92464eabb3.

--- pkgs/applications/editors/microej-sdk/4.1.5.nix
+++
@@ -1,8 +0,0 @@
-{ openjdk8, gtk2, callPackage }:
-
-callPackage (import ./common.nix rec {
-  version = "4.1.5";
-  sha256 = "sha256-EaaukOzFwUB6A8vBneF1JDIFrH/ENu/EFjQkITdU518=";
-  jdk = openjdk8;
-  gtk = gtk2;
-}) {}

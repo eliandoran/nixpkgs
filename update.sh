d9991ebedbbd515bbeaefa762d791e5c0e173e89
Elian Doran <contact@eliandoran.me>
2023-05-26 18:48:26 +0300

    Add update script

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+UPSTREAM_BRANCH="nixos-23.05"
+OWN_BRANCH="own-${UPSTREAM_BRANCH}"
+
+# Move to the correct branch.
+git checkout $OWN_BRANCH
+git pull
+
+# Update the branch with upstream.
+git fetch upstream
+git merge upstream/$UPSTREAM_BRANCH --no-edit
+git push

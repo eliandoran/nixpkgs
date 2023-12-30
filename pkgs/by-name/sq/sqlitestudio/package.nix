3c8a2ff68ce2ce173dbe17d359afc8923b9851d9
Elian Doran <contact@eliandoran.me>
2023-12-30 18:08:23 +0200

    Make it work

--- pkgs/by-name/sq/sqlitestudio/package.nix
+++ pkgs/by-name/sq/sqlitestudio/package.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, pkg-config, libsForQt5 }:
+{ stdenv, fetchFromGitHub, pkg-config, libsForQt5, makeWrapper, readline }:
 
 stdenv.mkDerivation rec {
   pname = "sqlitestudio";
@@ -12,12 +12,22 @@ stdenv.mkDerivation rec {
     hash = "sha256-5oBYv8WxyfVvvqr15XApvn6P/lBxR8b6E+2acRkvX0U=";
   };
 
+  buildInputs = [
+    libsForQt5.qtbase
+    libsForQt5.qttools
+    readline
+  ];
+
   nativeBuildInputs = [
+    makeWrapper
     pkg-config
     libsForQt5.wrapQtAppsHook
     libsForQt5.qmake
   ];
 
-  qmakeFlags = [ "SQLiteStudio3/SQLiteStudio3.pro" "DESTDIR=${placeholder "out"}/bin" ];
+  qmakeFlags = [
+    "PREFIX=${placeholder "out"}"
+    "SQLiteStudio3/SQLiteStudio3.pro"
+  ];
 
 }

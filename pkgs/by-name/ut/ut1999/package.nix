349f796c7e01bd49987540d4608be04c5f4f9d21
Elian Doran <contact@eliandoran.me>
2024-03-26 22:56:32 +0200

    ut1999: add support for i686-linux

--- pkgs/by-name/ut/ut1999/package.nix
+++ pkgs/by-name/ut/ut1999/package.nix
@@ -25,10 +25,16 @@ in stdenv.mkDerivation rec {
   name = "ut1999";
   version = "469d";
   sourceRoot = ".";
-  src = fetchurl {
-    url = "https://github.com/OldUnreal/UnrealTournamentPatches/releases/download/v${version}/OldUnreal-UTPatch${version}-Linux-amd64.tar.bz2";
-    hash = "sha256-aoGzWuakwN/OL4+xUq8WEpd2c1rrNN/DkffI2vDVGjs=";
-  };
+  src = {
+    x86_64-linux = fetchurl {
+      url = "https://github.com/OldUnreal/UnrealTournamentPatches/releases/download/v${version}/OldUnreal-UTPatch${version}-Linux-amd64.tar.bz2";
+      hash = "sha256-aoGzWuakwN/OL4+xUq8WEpd2c1rrNN/DkffI2vDVGjs=";
+    };
+    i686-linux = fetchurl {
+      url = "https://github.com/OldUnreal/UnrealTournamentPatches/releases/download/v${version}/OldUnreal-UTPatch${version}-Linux-x86.tar.bz2";
+      hash = "sha256-1JsFKuAAj/LtYvOUPFu0Hn+zvY3riW0YlJbLd4UnaKU=";
+    };
+  }.${stdenv.hostPlatform.system} or (throw "unsupported system: ${stdenv.hostPlatform.system}");
 
   buildInputs = [
     libgcc
@@ -47,34 +53,40 @@ in stdenv.mkDerivation rec {
     imagemagick
   ];
 
-  installPhase = ''
-    runHook preInstall
+  installPhase =
+    let
+      systemDir = {
+        x86_64-linux = "System64";
+        i686-linux = "System";
+      }.${stdenv.hostPlatform.system} or (throw "unsupported system: ${stdenv.hostPlatform.system}");
+    in ''
+      runHook preInstall
 
-    mkdir -p $out/bin
-    cp -r ./* $out
+      mkdir -p $out/bin
+      cp -r ./* $out
 
-    # Remove bundled libraries to use native versions instead
-    rm $out/System64/libmpg123.so* \
-      $out/System64/libopenal.so* \
-      $out/System64/libSDL2* \
-      $out/System64/libxmp.so*
+      # Remove bundled libraries to use native versions instead
+      rm $out/${systemDir}/libmpg123.so* \
+        $out/${systemDir}/libopenal.so* \
+        $out/${systemDir}/libSDL2* \
+        $out/${systemDir}/libxmp.so*
 
-    chmod -R 755 $out
+      chmod -R 755 $out
 
-    ln -s ${unpackGog}/Music $out
-    ln -s ${unpackGog}/Sounds $out
-    cp -n ${unpackGog}/Textures/* $out/Textures || true
-    ln -s ${unpackGog}/Maps $out
-    cp -n ${unpackGog}/System/*.{u,int} $out/System || true
+      ln -s ${unpackGog}/Music $out
+      ln -s ${unpackGog}/Sounds $out
+      cp -n ${unpackGog}/Textures/* $out/Textures || true
+      ln -s ${unpackGog}/Maps $out
+      cp -n ${unpackGog}/System/*.{u,int} $out/System || true
 
-    ln -s "$out/System64/ut-bin" "$out/bin/ut1999"
-    ln -s "$out/System64/ucc-bin" "$out/bin/ut1999-ucc"
+      ln -s "$out/${systemDir}/ut-bin" "$out/bin/ut1999"
+      ln -s "$out/${systemDir}/ucc-bin" "$out/bin/ut1999-ucc"
 
-    convert "${unpackGog}/gfw_high.ico" "ut1999.png"
-    install -D ut1999-5.png "$out/share/icons/hicolor/256x256/apps/ut1999.png"
+      convert "${unpackGog}/gfw_high.ico" "ut1999.png"
+      install -D ut1999-5.png "$out/share/icons/hicolor/256x256/apps/ut1999.png"
 
-    runHook postInstall
-  '';
+      runHook postInstall
+    '';
 
   desktopItems = [
     (makeDesktopItem {
@@ -90,7 +102,7 @@ in stdenv.mkDerivation rec {
   meta = with lib; {
     description = "Unreal Tournament GOTY (1999) with the OldUnreal patch.";
     license = licenses.unfree;
-    platforms = [ "x86_64-linux" ];
+    platforms = [ "x86_64-linux" "i686-linux" ];
     maintainers = with maintainers; [ eliandoran ];
     sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
     mainProgram = "ut1999";
